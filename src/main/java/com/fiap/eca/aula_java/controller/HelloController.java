b438730aaf42db0f0b4315205e1613340a24c11b
luccc777 <lucsouza7@gmail.com>
2025-02-21 09:00:17 -0300

    Aula java - Execício

--- src/main/java/com/fiap/eca/aula_java/controller/HelloController.java
+++ src/main/java/com/fiap/eca/aula_java/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping
     public String sayHello() {
-        return "Olá, Spring Boot!";
+        return "Olá, Spring Boot!!";
     }
 }
